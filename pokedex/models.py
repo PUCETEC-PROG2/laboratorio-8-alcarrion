9c3fbd5508e3ef928cee0ea9a50044bc205dd6a6
pucetec <alcarrion@puce.edu.ec>
2024-07-06 10:10:56 -0500

    Añado display, views, index, urls

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon (models.Model):
+    name= models.CharField(max_length=30, null=False)
+    type= models.CharField(max_length=30, null=False)
+    weigth = models.IntegerField(null=False)
+    height = models.IntegerField(null=False)
+    
+    def __str__(self) -> str:
+        return self.name
+
